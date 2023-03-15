15f3576539bc0adbcc667f6063b7f3627a57dff4
Vlad Kuzmin <Vlad.Kuzmin@26-agency.com>
2023-03-15 09:23:20 +0000

    [DS]

--- src/design-system/js/components/navigation.ts
+++ src/design-system/js/components/navigation.ts
@@ -1,13 +1,16 @@
 export const navigation = (content: any) => {
     return `
         <a href="/">Homepage</a>
-        ${Object.keys(content).map((key) => `
-            <h3>${key}</h3>
-            <ul>
-                ${content[key].map((item: any) => `
-                    <li><a href="${item.url}">${item.title}</a></li>
-                `).join('')}
-            </ul>
+        ${Object.keys(content).map((key, id) => `
+            <div class="ds-nav-category">
+                <input id="cat-${id}" type="checkbox"/>
+                <label for="cat-${id}">${key}</label>
+                <ul>
+                    ${content[key].map((item: any) => `
+                        <li><a href="${item.url}">${item.title}</a></li>
+                    `).join('')}
+                </ul>
+            </div>
         `).join('')}
         <div class="ds-close"></div>
     `
